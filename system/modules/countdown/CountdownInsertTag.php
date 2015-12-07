d9045a4e60077b232cc98637f60f80744b412dc7
Leo Unglaub <leo@leo-unglaub.net>
2015-12-07 12:38:37 +0100

    Update the copyright and the coding style
    Signed-off-by: Leo Unglaub <leo@leo-unglaub.net>

--- system/modules/countdown/CountdownInsertTag.php
+++ system/modules/countdown/CountdownInsertTag.php
@@ -2,11 +2,11 @@
 
 /**
  * Countdown
- * Copyright (C) 2013 2create
+ * Copyright (C) 2015 Foxship Technologies
  *
  * @package	countdown
- * @license	LGPL
- * @link	http://www.2create.at
+ * @license	GPL
+ * @link	https://www.foxship.com
  */
 
 
@@ -26,20 +26,18 @@ class CountdownInsertTag
 	 * @param	string			$strTag		The unknown insert tag.
 	 * @return	string|false				The value for the insert tag or false.
 	 */
-	public function replaceInsertTags($strTag)
+	public function replaceInsertTags ($strTag)
 	{
-		$arrChunks = trimsplit('::', $strTag);
+		$arrChunks = trimsplit ('::', $strTag);
 
 		// check if we have to parse the insert tag
 		if ($arrChunks[0] == 'countdown')
 		{
-			$objCountdown = new Countdown($arrChunks[1], $arrChunks[2]);
-			return $objCountdown->getDifference($arrChunks[3]);
+			$objCountdown = new Countdown ($arrChunks[1], $arrChunks[2]);
+			return $objCountdown->getDifference ($arrChunks[3]);
 		}
 
 		// not our insert tag, so we ignore it
 		return false;
 	}
 }
-
-?>
