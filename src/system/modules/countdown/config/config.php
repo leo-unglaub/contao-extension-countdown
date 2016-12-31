34fea40a9d01143f1e77f64fc0c17612645db41d
Leo Unglaub <leo@unglaub.at>
2016-12-31 16:48:53 +0100

    Remove useless comments and use the short array syntax
    Signed-off-by: Leo Unglaub <leo@unglaub.at>

--- src/system/modules/countdown/config/config.php
+++ src/system/modules/countdown/config/config.php
@@ -1,16 +1,6 @@
 <?php
 
 /**
- * Countdown
- * Copyright (C) 2015 Foxship Technologies
- *
- * @package	countdown
- * @license	GPL
- * @link	https://www.foxship.com
- */
-
-
-/**
  * Hooks
  */
-$GLOBALS['TL_HOOKS']['replaceInsertTags'][] = array ('CountdownInsertTag', 'replaceInsertTags');
+$GLOBALS['TL_HOOKS']['replaceInsertTags'][] = ['CountdownInsertTag', 'replaceInsertTags'];
